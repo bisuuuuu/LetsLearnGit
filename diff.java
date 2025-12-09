001ff07a2ced155944ab250927aefac5d62914b2
biswa <biswaprakash1836@gmail.com>
2025-12-09 15:22:40 +0530

    modified diff java file

---
+++ diff.java
@@ -0,0 +1,4 @@
+public class diff {
+public static void main(String[] args) {
+    System.out.println("The two files differ in their operations: multiply.java performs multiplication while q1.java performs addition.");
+}}
