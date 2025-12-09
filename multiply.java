cf58f23d65ebe9779c66aee1d265a158a0ad7ae2
biswa <biswaprakash1836@gmail.com>
2025-12-09 13:43:31 +0530

    multiply added

---
+++ multiply.java
@@ -0,0 +1,12 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class multiply {
+   public multiply() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 12;
+      byte var2 = 13;
+      int var3 = var1 * var2;
+      System.out.println("the res is" + var3);
+   }
+}
